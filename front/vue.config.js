85eefa96b26cee89c745970b2cfee8459e11b2a8
Zhi-hi <2500561808@qq.com>
2022-07-08 12:02:10 +0800

    1

--- front/vue.config.js
+++ front/vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     proxy: {                 //设置代理，必须填
       '/api': {//设置拦截器  拦截器格式   斜杠+拦截器名字，名字可以自己定
         ws:true,
-        target: 'http://121.40.153.2:5000',     //代理的目标地址
+        target: 'http://10.128.162.255:5000',     //代理的目标地址
         changeOrigin: true,              //是否设置同源，输入是的
         pathRewrite: {                   //路径重写
           '^/api': ''                     //选择忽略拦截器里面的内容
